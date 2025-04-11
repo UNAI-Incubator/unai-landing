23e5661d25b350698bd1921b4d01c9fafe3ca357
UNAI-Incubator <YSKAURAV@GMAIL.COM>
2025-04-11 11:01:27 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,12 @@
+
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import UNAILandingPage from "./App";
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(
+  <React.StrictMode>
+    <UNAILandingPage />
+  </React.StrictMode>
+);
