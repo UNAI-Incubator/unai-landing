767ab3804d21287671ae5c8023d4f65189d99dd1
UNAI-Incubator <YSKAURAV@GMAIL.COM>
2025-04-11 12:17:42 +0530

    Add files via upload

---
+++ App.jsx
@@ -0,0 +1,9 @@
+
+export default function App() {
+  return (
+    <div className='text-center py-10'>
+      <h1 className='text-4xl font-bold text-white'>UNAI Landing Page</h1>
+      <p className='text-gray-400 mt-4'>Your final design goes here.</p>
+    </div>
+  )
+}
