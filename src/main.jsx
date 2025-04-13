e2519398211168ac93b7025f71dc90034f888b11
UNAI-Incubator <YSKAURAV@GMAIL.COM>
2025-04-13 19:59:59 +0530

    Trial 3

--- src/main.jsx
+++ src/main.jsx
@@ -2,10 +2,10 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import './index.css'
-import App from './App.jsx'
+import UNAILandingPage from './App'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    <UNAILandingPage />
   </React.StrictMode>
 )
